c508efc129699715c62b5845f0e9703281df9f11
aoddy10 <vobi69p@gmail.com>
2023-10-01 13:58:23 +0700

    add: button action for hero section.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -44,6 +44,9 @@ export default function Hero() {
         <motion.p
           initial={{ opacity: 0, y: 100 }}
           animate={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.1,
+          }}
           className=" font-normal text-2xl mt-4 !leading-10"
         >
           I possess a diverse range of technical skills. With a strong aptitude
@@ -52,26 +55,47 @@ export default function Hero() {
         </motion.p>
       </div>
 
-      <div className="flex flex-col sm:flex-row justify-center gap-2 px-4 text-lg font-medium">
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: 0.2,
+          },
+        }}
+        className="flex flex-col sm:flex-row justify-center gap-3 px-4 text-lg font-medium"
+      >
         <Link
           href="#contact"
-          className=" bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full"
+          className=" group  bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-grey-950 active:scale-105 transition"
         >
-          Contact me here <BsArrowRight />
+          Contact me here{" "}
+          <BsArrowRight className=" opacity-70 group-hover:translate-x-1 transition" />
         </Link>
-        <a className=" bg-white px-7 py-3 flex items-center gap-2 rounded-full">
-          Download CV <HiDownload />
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-grey-950 active:scale-105 transition cursor-pointer border border-black/10 "
+          href="/Anirut_Puangkingkaew_CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className=" opacity-60 group-hover:translate-y-1 transition" />
         </a>
         <a
-          className=" bg-white p-4 text-gray-700 flex items-center 
-         gap-2 rounded-full"
+          className=" bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-125 hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://linkedin.com/in/anirutpuangkingkaew"
+          target="_blank"
         >
           <BsLinkedin />
         </a>
-        <a className=" bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full">
+        <a
+          className=" bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-125 hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://github.com/aoddy10"
+          target="_blank"
+        >
           <FaGithubSquare />
         </a>
-      </div>
+      </motion.div>
     </section>
   );
 }
