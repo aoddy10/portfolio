a7ddadb9a755f6e5c396613a769ab6e2ec4d1083
aoddy10 <vobi69p@gmail.com>
2023-10-02 08:46:43 +0700

    add: display project detail for each one and style project section.

--- src/components/section-header.tsx
+++ src/components/section-header.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useScroll } from "framer-motion";
 
 type SectionHeaderProps = {
   children: React.ReactNode;
 };
 
 function SectionHeader({ children }: SectionHeaderProps) {
-  return <h2 className=" font-medium text-3xl capitalize mb-8">{children}</h2>;
+  return (
+    <h2 className=" font-medium text-3xl capitalize mb-8 text-center">
+      {children}
+    </h2>
+  );
 }
 
 export default SectionHeader;
