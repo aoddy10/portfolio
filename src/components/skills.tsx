a1fe495ac2731e492f734d6e96f0f81d58071760
aoddy10 <vobi69p@gmail.com>
2023-10-06 11:26:43 +0700

    style my skills section to delay display skill for each one.

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -4,6 +4,18 @@ import React from "react";
 import SectionHeader from "./section-header";
 import { skillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks/useSectionInView";
+import { motion } from "framer-motion";
+
+const fadeAnimationsVariants = {
+  initial: { opacity: 0, y: 100 },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
 
 function Skills() {
   const { ref } = useSectionInView("Skills");
@@ -11,19 +23,26 @@ function Skills() {
   return (
     <section
       ref={ref}
-      id="Skills"
+      id="skills"
       className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
     >
       <SectionHeader>My Skills</SectionHeader>
 
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
-          <li
+          <motion.li
             className="bg-white border border-black/10 rounded-xl px-5 py-3"
             key={index}
+            variants={fadeAnimationsVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
           >
             {skill}
-          </li>
+          </motion.li>
         ))}
       </ul>
     </section>
