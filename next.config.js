9577f8337d50279a29b410b80f813188095a69f1
aoddy10 <vobi69p@gmail.com>
2023-10-15 16:09:06 +1100

    update deploy files.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  // output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
