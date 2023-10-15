8e1c32576be48ea128f071123b493d2a78358f1a
aoddy10 <vobi69p@gmail.com>
2023-10-15 15:05:57 +1100

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+<<<<<<< HEAD
   experimental: {
     serverActions: true,
   },
+=======
+  // experimental: {
+  //   serverActions: true,
+  // },
+>>>>>>> parent of 34b4fbb (fix: deploy error)
 };
 
 module.exports = nextConfig;
