2e6a82a3ce41772fc607a2a38ff820bd6a111f1a
aoddy10 <vobi69p@gmail.com>
2023-10-23 18:02:29 +1100

    update projects.

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,9 +2,11 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "../../public/corpcomment.png"
-import rmtdevImg from "../../public/rmtdev.png";
-import wordanalyticsImg from "../../public/wordanalytics.png";
+
+import emergencyAlertApp from '../../public/emergency-alert.png'
+import massageBookingApp from '../../public/massage-booking-app.png'
+import smmsReportApp from '../../public/smms-report.png'
+import projectManagementApp from '../../public/weera-wissawakan.png'
 
 export const links = [
   {
@@ -64,23 +66,30 @@ export const projectsData = [
   {
     title: "People collapsed detection and notify system.",
     description:
-      "I possess knowledge and skills in AI and Machine Learning, showcased through my active participation in a Machine Learning project. This project, implemented in Python, focused on utilizing machine learning techniques to analyse camera footage, specifically Raspberry Pi cameras, to detect instances of people falling or collapsing. I employed TensorFlow to train the machine learning model, enabling it to accurately identify such events. To enhance user safety, the system was designed to send real-time notifications to alert users about potential emergencies. The recorded data from the system was securely stored in Firebase, which facilitated the delivery of timely notifications to a mobile application developed with Flutter. This project underscores my proficiency in applying machine learning for real-time event detection and my ability to integrate multiple technologies to create a comprehensive and potentially lifesaving solution.",
+      "In a Python project, I showcased my AI and Machine Learning skills by utilizing TensorFlow to detect falls in Raspberry Pi camera footage and send real-time alerts through a Flutter app.",
     tags: ["Python", "Tensorflow", "Firebase", "Raspberry-PI", "Open-CV"],
-    imageUrl: corpcommentImg,
+    imageUrl: emergencyAlertApp,
   },
   {
     title: "Massage Booking Management System",
     description:
-      "I have successfully designed and developed a mobile application for a massage business booking system, leveraging the power of React Native for cross-platform compatibility. The app empowers managers with seamless booking slot management and customer issue recording capabilities. Additionally, it offers business owners robust tools for payment management and in-depth business analysis, enabling data-driven decisions. Furthermore, the app facilitates customer relationship management (CRM) and targeted advertising by leveraging the data recorded, thereby boosting business growth. This project exemplifies my proficiency in mobile app development and my ability to create multifunctional solutions that enhance operational efficiency and business expansion while adhering to industry best practices.",
+      "Utilizing React Native, I designed a cross-platform mobile app for a massage business, simplifying booking and issue recording. It provides payment management and analytics tools, empowering data-driven decisions.",
     tags: ["React Native", "Tailwind", "Redux", "NodeJS", "Express Framework", "JWT Authentication", "MongoDB", "Docker container", "Git Version Control"],
-    imageUrl: rmtdevImg,
+    imageUrl: massageBookingApp,
+  },
+  {
+    title: "Project management System",
+    description:
+      "Created a project management system with ReactJS, Node.js, and MySQL integration, empowering managers with performance analysis and cost tools, highlighting my full-stack development expertise.",
+    tags: ["React", "SQL", "MUI", "NodeJS", "Express framework", "JWT Authentication", "Docker container", "Git Version Control" ],
+    imageUrl: smmsReportApp,
   },
   {
     title: "Student Management System",
     description:
-      "I bring extensive experience in the development of a comprehensive Student Management System(SMS) using AwareIM. Additionally, I've extended its functionality by creating the SMMS Report, a dynamic reporting tool, utilizing ReactJS and Node.js Express Framework, and seamlessly connecting it to an MSSQL Database. This innovative reporting system significantly enhanced management efficiency. Moreover, I've integrated the SMMS Report with both the Student Management System and Moodle, the E-learning system, facilitating automated data transfer through Web Service API functions. Throughout the development process, I adopted Docker Container for efficient deployment and Git Version control for organized and collaborative development. This multifaceted project not only reflects my technical proficiency but also underscores my capacity to design and integrate complex systems while promoting efficient data management and reporting.",
+      "Developed a Student Management System using AwareIM, expanded it with the SMMS Report via ReactJS and Node.js, and integrated it with Moodle for data transfer efficiency. Docker and Git were employed for deployment and collaboration.",
     tags: ["React", "SQL", "MUI", "NodeJS", "Express framework", "JWT Authentication", "Docker container", "Git Version Control" ],
-    imageUrl: wordanalyticsImg,
+    imageUrl: smmsReportApp,
   },
 ] as const;
 
