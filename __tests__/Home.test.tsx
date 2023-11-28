4eb636799092eafc209de732130add53dd380ecc
aoddy10 <vobi69p@gmail.com>
2023-11-28 14:12:47 +1100

    update cv

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -2,9 +2,12 @@ import { render, screen } from "@testing-library/react";
 import Home from "@/app/page";
 
 it("should have hero section", () => {
+  // Arrange
   render(<Home />);
 
+  // Action
   const myElement = screen.getByText("Hello, I'm Anirut.");
 
+  // Assert
   expect(myElement).toBeInTheDocument();
 });
