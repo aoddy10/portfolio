77a672e3c421984aae9f8a02c459eb019c1f7571
aoddy10 <vobi69p@gmail.com>
2024-06-01 11:49:04 +1000

    Add carousel page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/about";
+import CarouselPage from "@/components/carousel";
 import Contact from "@/components/contact";
 import Divider from "@/components/divider";
 import Experience from "@/components/experience";
@@ -9,13 +10,15 @@ import Skills from "@/components/skills";
 export default function Home() {
   return (
     <main className=" flex flex-col items-center px4">
-      <Hero />
+      <CarouselPage />
+
+      {/* <Hero />
       <Divider />
       <About />
       <Projects />
       <Skills />
       <Experience />
-      <Contact />
+      <Contact /> */}
     </main>
   );
 }
