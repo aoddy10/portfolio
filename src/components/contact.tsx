2de41f6e320e8dedca321ebdab8150e75737a677
Anirut <vobi69p@gmail.com>
2025-06-24 22:29:53 +1200

    Disable contact form. Update experiences.

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -9,46 +9,49 @@ import SubmitBtn from "./submit-btn";
 import toast from "react-hot-toast";
 
 type contactFormType = {
-  email: string;
-  message: string;
+    email: string;
+    message: string;
 };
 
 function Contact() {
-  const [data, setData] = useState<contactFormType>({ email: "", message: "" });
+    const [data, setData] = useState<contactFormType>({
+        email: "",
+        message: "",
+    });
 
-  return (
-    <motion.section
-      id="contact"
-      className="mb-28 px-2 sm:mb-28 scroll-mt-28 w-[min(100%,38rem)] flex flex-col items-center"
-      initial={{
-        opacity: 0,
-      }}
-      whileInView={{
-        opacity: 1,
-      }}
-      transition={{
-        duration: 1,
-      }}
-      viewport={{
-        once: true,
-      }}
-    >
-      <SectionHeader>Contact</SectionHeader>
-
-      <p className=" text-white font-thin -mt-6 dark:text-white/80">
-        Please contact me at email:{" "}
-        <a
-          className="underline font-semibold ml-2"
-          href="mailto:anirut.puangkingkaew@gmail.com"
-          aria-label="Contact me by sending email"
+    return (
+        <motion.section
+            id="contact"
+            className="mb-28 px-2 sm:mb-28 scroll-mt-28 w-[min(100%,38rem)] flex flex-col items-center"
+            initial={{
+                opacity: 0,
+            }}
+            whileInView={{
+                opacity: 1,
+            }}
+            transition={{
+                duration: 1,
+            }}
+            viewport={{
+                once: true,
+            }}
         >
-          anirut.puangkingkaew@gmail.com
-        </a>
-        <br />
-        or through the contact form below.
-      </p>
+            <SectionHeader>Contact</SectionHeader>
 
-      <form
+            <p className=" text-white font-thin -mt-6 dark:text-white/80">
+                Please contact me at email:{" "}
+                <a
+                    className="underline font-semibold ml-2"
+                    href="mailto:anirut.puangkingkaew@gmail.com"
+                    aria-label="Contact me by sending email"
+                >
+                    anirut.puangkingkaew@gmail.com
+                </a>
+                {/* <br />
+                or through the contact form below. */}
+            </p>
+
+            {/* <form
         action={async (formData) => {
           const { data, error } = await sendEmail(formData);
 
@@ -89,9 +92,9 @@ function Contact() {
         />
 
         <SubmitBtn />
-      </form>
-    </motion.section>
-  );
+      </form> */}
+        </motion.section>
+    );
 }
 
 export default Contact;
